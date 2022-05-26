8d92be401cad966084c73bcd3b8889908f577331
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-26 11:25:32 +0530

    viewing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addstudent from './Components/Addstudent';
+import Viewstudent from './Components/Viewstudent';
 
 function App() {
   return (
     <div>
       <Addstudent/>
+      <Viewstudent/>
     </div>
   );
 }
